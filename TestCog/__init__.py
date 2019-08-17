e8be2031e44ded7f2eb2fcfe005b9366df199efd
EpicElric <EpicElric@users.noreply.github.com>
2019-08-16 23:33:24 -0700

    updates

--- TestCog/__init__.py
+++ TestCog/__init__.py
@@ -1,4 +1,4 @@
 from .mycog import Mycog
 
 def setup(bot):
-    bot.add_cog(Mycog())
+    bot.add_cog(Mycog(bot))
--- TestCog/mycog.py
+++ TestCog/mycog.py
@@ -2,14 +2,31 @@ import discord
 import gspread
 import csv
 import asyncio
+
+import httplib2
+
 from datetime import datetime
 from redbot.core import commands
 from redbot.core.data_manager import bundled_data_path
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from oauth2client.service_account import ServiceAccountCredentials
+
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload
+
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+import io
+
 from redbot.core.utils.predicates import ReactionPredicate
 from redbot.core.utils.predicates import MessagePredicate
 from redbot.core.utils.menus import start_adding_reactions
+
+from discord.utils import get
 
 
 # use creds to create a client to interact with the Google Drive API
@@ -21,48 +38,95 @@ from redbot.core.utils.menus import start_adding_reactions
 
 class Mycog(commands.Cog):
     """My custom cog"""
-
+    def __init__(self, bot):
+        self.bot = bot
 
     @commands.command()
+    @commands.has_any_role("BossMan")
     async def test(self, ctx):
-        """Test Code"""
+        await ctx.send('Has Role')
+
+    @commands.command()
+    @commands.has_any_role("BossMan")
+    async def lookup2(self, ctx, *, name):
+        """Lookup contact info"""
         # Your code will go here
+        member = ctx.author
+        message = await member.send('Fetching Information, Please Wait')
+
         scope = ['https://spreadsheets.google.com/feeds',
                 'https://www.googleapis.com/auth/drive']
         file_path = bundled_data_path(self) / 'client_secret.json'
         creds = ServiceAccountCredentials.from_json_keyfile_name(file_path, scope)
         client = gspread.authorize(creds)
-        sheet = client.open("DragonBot Tester").sheet1
-        row = ["I'm","inserting","a","row","into","a,","Spreadsheet","with","Python"]
-        index = 2
-        sheet.insert_row(row, index)
+        sheet = client.open("Draconian Fleet Contact Sheet").sheet1
+
+
+        try:
+            cell = sheet.find(name.capitalize())
+            row = cell.row
+            values_list = sheet.row_values(row)
+            
+            discordid = discord.Member
+
+            fname = values_list[0]
+            lname = values_list[1]
+            pname = values_list[3]
+            dob = values_list[4]
+            email = values_list[5]
+            phone = values_list[6]
+            
+            ship = values_list[7]
+            rank = values_list[8]
+            homeaddr = values_list[9]
+            mailaddr = values_list[10]
+            paypale = values_list[11]
+            prefcont = values_list[12]
+            allerg1 = values_list[13]
+            allerg2 = values_list[14]
+            cname = values_list[15]
+            cphone = values_list[16]
+            crel = values_list[17]
+            faceurl = values_list[18]
+            discordid = values_list[19]
+            updated = values_list[20]
+            wname = fname + ' ' + lname
+            userid = self.bot.get_user(int(discordid))
+            discordav = userid.avatar_url
+            discordhandle = userid.name
+
+            contact1 = '**Pirate Name:** ' + pname + '\n**Phone:** ' + phone + '\n**Email:** ' + email + '\n**Discord:** ' + discordhandle
+            contact2 = '**Name:** ' + cname + '\n**Phone:** ' + cphone + '\n**Relationship:** ' + crel
+            contact3 = '**Medical Allergies:** ' + allerg1 + '\n**Food Allergies:** ' + allerg2
+            contact4 = '**Birthday:** ' + dob
+            contact5 = '**Ship:** ' + ship + '\n**Rank:** ' + rank
+
+            embed = discord.Embed(colour=discord.Colour(0x106bca))
+            embed.set_author(name=wname, icon_url="https://i2.wp.com/www.draconianfleet.com/wp-content/uploads/2017/09/cropped-frame-DF2-1.png?w=250&ssl=1")
+            embed.set_thumbnail(url=discordav)
+            embed.set_image(url=faceurl)
+            embed.add_field(name="Contact Info", value=contact1, inline=True)
+            embed.add_field(name="Emergency Contact", value=contact2, inline=True)
+            embed.add_field(name="Allergies", value=contact3, inline=True)
+            embed.add_field(name="Personal", value=contact4, inline=True)
+            embed.add_field(name="Fleet Alignment", value=contact5, inline=False)
+            
+            await message.edit(content="", embed=embed)
+
+        except gspread.exceptions.CellNotFound:
+            await message.edit('Name not found')
 
     @commands.command()
-    async def cmd(self, ctx):
-        msg = await ctx.send("Yes or no?")
-        start_adding_reactions(msg, ReactionPredicate.YES_OR_NO_EMOJIS)
-        
-        pred = ReactionPredicate.yes_or_no(msg, ctx.author)
-        await ctx.bot.wait_for("reaction_add", check=pred)
-        if pred.result is True:
-        # User responded with tick
-            await ctx.send('yes')
-        else:
-        # User responded with cross
-            await ctx.send('no')
-    
-    
-    @commands.command()
-    async def cmd2(self, ctx):
-        msg = await ctx.send("React to me!")
-        emojis = ReactionPredicate.ALPHABET_EMOJIS[:5]
-        start_adding_reactions(msg, emojis)
+    async def avatar(self, ctx):
+        """Shows your Avatar"""
+        idnum = ctx.author.id
+        user = self.bot.get_user(int(idnum))
+        u = user.avatar_url    
+        embed = discord.Embed(description=user.name, timestamp=user.created_at ,colour=discord.Colour.blue())
+        embed.set_image(url=u)
 
-        pred = ReactionPredicate.with_emojis(emojis, msg)
-        await ctx.bot.wait_for("reaction_add", check=pred)
-        # pred.result is now the index of the letter in `emojis`
-        await ctx.send(pred.result)
-    
+        await ctx.send(embed=embed)
+
     @commands.command()
     async def cmd3(self, ctx):
         def check(m):
